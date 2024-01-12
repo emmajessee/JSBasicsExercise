97fe8864eefad4e02ff4e1369c657d721b8e350b
Student <student@BCCSs-MacBook-Pro.local>
2024-01-12 10:53:07 -0600

    1/21/24 Assignment

---
+++ script.js
@@ -0,0 +1,24 @@
+//1. 
+console.log(23);
+
+//2. 
+//Numbers, Strings, Booleans, Null, Undefined
+
+//3.
+//+, -, /, *, **, %
+
+//4. 
+console.log(1 % 5); // 1
+console.log(5 % 1); // 0
+
+//5. 
+console.log("I'M MARY POPPINS, Y'ALL!");
+
+//6. 
+console.log("Hawk" + "eye");
+
+//7. 
+console.log("Emma".length + "Jessee". length); //10 
+
+//8. 
+console.log("Avengers: Endgame" [16]);
